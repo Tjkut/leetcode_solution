6283fa8a8e6ea5c67c6f65e4ecb9c6a9efb70c19
tinhcmus <164772025+Tjkut@users.noreply.github.com>
2025-10-27 17:19:10 +0700

    Add bubble sort implementation in bubble_sort.c
    Implement bubble sort algorithm to sort an array.

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void bubbleSort(int arr[], int size_arr)
+{
+    bool checkswap = false;
+    int last_unsort_index = size_arr - 1;
 
+    do
+    {
+        checkswap=false;
+        for (int i = 0; i < last_unsort_index ; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                checkswap = true;
+                swap(&arr[i], &arr[i + 1]);
+            }
+        }
+        last_unsort_index--;
+    }
+    while(checkswap);
+}
+int main()
+{
+  int arr[] = {5, 5, 4, 3, 4, 2, 1};
+    int size_arr = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, size_arr);
+    printf("Sorted array: \n");
+    for (int i = 0; i < size_arr; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
